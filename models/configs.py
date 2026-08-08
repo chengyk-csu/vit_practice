663325345494fbb916311b0c13d5c47d192accaa
chengyk-csu <chengyikai.csu@gmail.com>
2026-08-08 11:45:34 +0800

    Fix ViT and hybrid configurations

--- models/configs.py
+++ models/configs.py
@@ -71,7 +71,7 @@ def H14Config():
     config.transformer = ml_collections.ConfigDict()
     config.transformer.mlp_dim = 5120
     config.transformer.attention_heads = 16
-    config.transformer.head_dim = 64
+    config.transformer.head_dim = 80
     config.transformer.num_layers = 32
     config.transformer.attn_dropout = 0.0
     config.transformer.dropout = 0.1
@@ -87,7 +87,7 @@ def get_testing_Config():
     config.transformer = ml_collections.ConfigDict()
     config.transformer.mlp_dim = 1
     config.transformer.attention_heads = 1
-    config.transformer.head_dim = 64
+    config.transformer.head_dim = 1
     config.transformer.num_layers = 1
     config.transformer.attn_dropout = 0.0
     config.transformer.dropout = 0.1
@@ -98,7 +98,7 @@ def get_testing_Config():
 def get_r50_b16_Config():
     config = ml_collections.ConfigDict()
     config.image_size = (224,224)
-    config.patches_grad = ml_collections.ConfigDict({"size":(14,14)})
+    config.patches = ml_collections.ConfigDict({"grid":(14,14)})
     config.hidden_size = 768
     config.transformer = ml_collections.ConfigDict()
     config.transformer.mlp_dim = 3072
